Corrected states storing in auditgm, dimensions in models, rnn part of VRNN, use of obs instead of ctx as input for training, added normalizing, save plot samples

RNN/model.py
@@ -138,14 +138,14 @@ class VAE(nn.Module):
                 
         if self.output_dim==2 and x_output.size(2)==2:
             # In case output contains mu and var
-            out_estim_mu    = x_output[:,:,0]
-            out_estim_var   = F.softplus(x_output[:,:, 1]) + 1e-6 # Ensure the variance is stricly positive
-            if kwargs: recon_loss = loss_func(out_estim_mu, x_target.view(-1, self.x_dim), out_estim_var, **kwargs)
-            else: recon_loss = loss_func(out_estim_mu, x_target.view(-1, self.x_dim), out_estim_var)
+            out_estim_mu    = x_output[:, :, [0]]
+            out_estim_var   = F.softplus(x_output[:, :, [1]]) + 1e-6 # Ensure the variance is stricly positive
+            if kwargs: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var, **kwargs)
+            else: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var)
         elif self.output_dim==1 and x_output.size(2)==1:
             x_output = torch.sigmoid(x_output)
-            if kwargs: recon_loss = loss_func(x_output, x_target.view(-1, self.x_dim), **kwargs) # in case dealing with point estimates and not distributions, # TODO: verify if want to keep BCE over MSE
-            else: recon_loss = loss_func(x_output, x_target.view(-1, self.x_dim))
+            if kwargs: recon_loss = loss_func(x_output, x_target, **kwargs) # in case dealing with point estimates and not distributions, # TODO: verify if want to keep BCE over MSE
+            else: recon_loss = loss_func(x_output, x_target)
         else:
             raise ValueError('output_dim must be 1 or 2') 
         
@@ -202,7 +202,7 @@ class VRNN(VAE): # Or rather, inherit from VAE
 
         # RNN part
         # self.recurrence = SimpleRNN(x_dim=self.phi_x_dim + self.phi_z_dim, output_dim=..., hidden_dim=self.rnn_hidden_states_dim, n_layers=self.rnn_n_layers, batch_size=batch_size)
-        self.rnn = nn.GRU(input_size=self.x_dim + self.latent_dim, hidden_size=self.rnn_hidden_states_dim, num_layers=self.rnn_n_layers, batch_first=True, device=device)
+        self.rnn = nn.GRU(input_size=self.phi_x_dim + self.phi_z_dim, hidden_size=self.rnn_hidden_states_dim, num_layers=self.rnn_n_layers, batch_first=False, device=device)
         # self.output_layer = nn.Linear(self.rnn_hidden_states_dim, x_dim, device=device)
 
 
@@ -219,7 +219,7 @@ class VRNN(VAE): # Or rather, inherit from VAE
         seq_len, batch_size, x_dim = x.size()
         if x_dim!=self.x_dim: raise ValueError("Incorrect dimensions for input x")
 
-        h_prev          = torch.zeros(self.rnn_n_layers, batch_size, self.rnn_hidden_states_dim, device=self.device)
+        h_prev  = torch.zeros(self.rnn_n_layers, batch_size, self.rnn_hidden_states_dim, device=self.device)
         outputs = []
         latent_mus = []
         latent_logvars = []
@@ -248,8 +248,8 @@ class VRNN(VAE): # Or rather, inherit from VAE
 
 
             # Decode
-            x_t_ = self.decoder(torch.cat([self.phi_z(z_t), h_prev[-1]], dim=-1)) # Eq. 6
-            outputs.append(x_t_)
+            x_t_out = self.decoder(torch.cat([self.phi_z(z_t), h_prev[-1]], dim=-1)) # Eq. 6
+            outputs.append(x_t_out)
             
             # Recurrence
             # rnn_output = self.recurrence(torch.cat([self.phi_x(x_t), self.phi_z(z_t)], dim=-1), h_prev) # Eq. 7
@@ -281,15 +281,15 @@ class VRNN(VAE): # Or rather, inherit from VAE
         
         # In case output contains mu and var
         if self.output_dim==2:
-            out_estim_mu = x_output[:, :, 0]
-            out_estim_var = F.softplus(x_output[:, :, 1]) + 1e-06
-            if kwargs: recon_loss = loss_func(out_estim_mu, x_target.view(-1, self.x_dim), out_estim_var, **kwargs)
-            else: recon_loss = loss_func(out_estim_mu, x_target.view(-1, self.x_dim), out_estim_var)
+            out_estim_mu = x_output[:, :, [0]]
+            out_estim_var = F.softplus(x_output[:, :, [1]]) + 1e-06
+            if kwargs: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var, **kwargs)
+            else: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var)
         # Or just point estimation
         elif self.output_dim==1 and x_output.size(2)==1:
             x_output = torch.sigmoid(x_output)
-            if kwargs: recon_loss = loss_func(x_output, x_target.view(-1, self.x_dim), **kwargs) # in case dealing with point estimates and not distributions, # TODO: verify if want to keep BCE over MSE
-            else: recon_loss = loss_func(x_output, x_target.view(-1, self.x_dim))
+            if kwargs: recon_loss = loss_func(x_output, x_target, **kwargs) # in case dealing with point estimates and not distributions, # TODO: verify if want to keep BCE over MSE
+            else: recon_loss = loss_func(x_output, x_target) # DELETED .view(-1, self.x_dim) from x_target.view(-1, self.x_dim)
         
         # KL divergence between two Gaussians per timestep & batch
         # KL(q||p) closed form between N(mu, var) and N(mu_prior, logvar_prior)

RNN/pipeline.py
@@ -10,6 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 DEVICE = torch.device('cpu')
+FREQ_MIN = 1400
+FREQ_MAX = 1650
 
 
 # TODO:
@@ -17,6 +19,28 @@ DEVICE = torch.device('cpu')
 # - save model weights
 # - load params
 # TODO: handle device switch, autograd...
+
+
+class MinMaxScaler():
+    def __init__(self):
+        self.freq_min = FREQ_MIN
+        self.freq_max = FREQ_MAX
+
+    def fit_normalize(self, x, freq_min=None, freq_max=None, margin=None):
+        if freq_min is None:
+            self.freq_min = x.min()
+        if freq_max is None:
+            self.freq_max = x.max()
+        if margin is not None:
+            self.freq_min -= margin
+            self.freq_max += margin
+        return self.normalize(x)
+
+    def normalize(self, x):
+        return (x - self.freq_min) / (self.freq_max - self.freq_min)
+    
+    def denormalize(self, x_norm):
+        return x_norm * (self.freq_max - self.freq_min) + self.freq_min
 
 
 
@@ -29,14 +53,15 @@ def train(model, n_batches, num_epochs, optimizer, lr_scheduler, gm_name, data_c
     else: raise ValueError("Invalid GM name")
 
     # Prepare to save the results
-    save_path = Path(f'./training_results/{model.name}/')
+    save_path = Path(f'/home/clevyfidel/Documents/Workspace/RNN_paradigm/training_results/{model.name}/')
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     # TODO CHECK-LIST
     # - a loss tolerance?
-    # - an early stopping in case of overfitting
+    # - an early stopping in case of overfitting (ABSOLUTELY according to Seyma) --> include IF i observe overfitting
     # - dropout?
+    # - assess need to change activation function or not (hand in hand with changing the models' inner layers' architecture)
     # - set requires_grad / autograd
     # x training + validation in eval mode
 
@@ -56,8 +81,12 @@ def train(model, n_batches, num_epochs, optimizer, lr_scheduler, gm_name, data_c
             optimizer.zero_grad()
 
             # Generate data (outside of train loop maybe?)
-            y, _, c = gm.generate_batch(N_batch=batch_size)
-            x = torch.tensor(y, dtype=torch.float, requires_grad=False).unsqueeze(2).to(DEVICE)
+            _, _, y = gm.generate_batch(N_batch=batch_size)
+            y = torch.tensor(y, dtype=torch.float, requires_grad=False).unsqueeze(2).to(DEVICE)
+
+            # Transform data
+            minmax = MinMaxScaler()
+            x = minmax.fit_normalize(y, margin=data_config["si_r"]+data_config["si_q"])
 
             # Call model
             model_output = model(x) # can contain output(, mu_latent, logvar_latent(, mu_prior, logvar_prior))
@@ -76,7 +105,7 @@ def train(model, n_batches, num_epochs, optimizer, lr_scheduler, gm_name, data_c
             if batch % batch_res == batch_res-1:
                 train_log_losses.append(float(loss.detach().cpu().item()))
                 lr = lr_scheduler.get_last_lr()[0]
-                loss_log(f'{save_path}/loss-{epoch}-{batch}', batch, batch_res, n_batches, tt, lr, loss.detach().cpu().item(), train_log_losses, plot=False)
+                loss_log(f'{save_path}/loss-{epoch}-{batch}', batch, batch_res, n_batches, tt, lr, loss.detach().cpu().item(), train_log_losses, plot=True)
                 tt = time.time()
             
         avg_train_loss = np.mean(train_losses)
@@ -90,8 +119,11 @@ def train(model, n_batches, num_epochs, optimizer, lr_scheduler, gm_name, data_c
                 valid_losses = []
 
                 # Generate data (outside of train loop maybe?)
-                y, _, c = gm.generate_batch(N_batch=batch_size)
-                x = torch.tensor(y, dtype=torch.float, requires_grad=False).unsqueeze(2).to(DEVICE)
+                _, _, y = gm.generate_batch(N_batch=batch_size)
+                y = torch.tensor(y, dtype=torch.float, requires_grad=False).unsqueeze(2).to(DEVICE)
+
+                # Transform data
+                x = minmax.normalize(y)
 
                 # Call model
                 model_output = model(x) # can contain output(, mu_latent, logvar_latent(, mu_prior, logvar_prior))
@@ -102,13 +134,15 @@ def train(model, n_batches, num_epochs, optimizer, lr_scheduler, gm_name, data_c
 
                 # Save valid samples
                 if batch % batch_res == batch_res-1:
-                    plot_samples(y, model_output, f'{save_path}/epoch-{epoch}_examples.png')
+                    plot_samples(y, minmax.denormalize(model_output), save_path=f'{save_path}/epoch-{epoch}_examples')
             
             
             avg_valid_loss = np.mean(valid_losses)
     
-    print(f"Epoch: {epoch + 1}, Training Loss: {avg_train_loss:.4f}, Valid Loss: {avg_valid_loss:.4f}")
+    print(f"Model: {model.name}, Epoch: {epoch + 1}, Training Loss: {avg_train_loss:.4f}, Valid Loss: {avg_valid_loss:.4f}")
+    
 
+    return minmax
  
 
 
@@ -133,22 +167,19 @@ def loss_log(logpath, batch, batch_res, n_batches, tt, lr, loss, losses, plot):
         Time at which the training for the current batch stated (as measured by time.time()) 
     lr        : float
         current learning rate
-    loss      : scalar torch.tensor
+    loss      : scalar torch.tensor (needs to be detached)
         total loss
 
     """
 
-    loss_np = loss.detach().item()
     sprint  = f'Batch {batch+1:>2}/{n_batches}; Time = {time.time()-tt:.1f}s; '
-    sprint += f'Loss = {loss_np:.3f} ('
-    slog = f'{loss_np/batch_res:.2f}'
-
+    sprint += f'Loss = {loss:.3f} ('
+    sprint += f'{loss/batch_res:.2f}'
     sprint += f'LR = {lr:.2g})'
 
     logfilename = f'{logpath}.txt'
     with open(logfilename, 'a') as f:
-        f.write(f'{slog}\n')
-
+        f.write(f'{sprint}\n')
 
     if plot:
         plotfile = f'{logpath}.png'
@@ -162,24 +193,62 @@ def plot_losses(losses, x_label, y_label, label, save_path):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.legend()
+
     plt.savefig(save_path)
     plt.close()
 
 
-def plot_samples(output, obs, ax, save_path):
+def plot_samples(obs, output, save_path):
+    obs = obs.squeeze()
     if isinstance(output, tuple): output = output[0]
-    if output.size(2) == 2: 
-        estim_mu, estim_sigma = output[:,:0], torch.sqrt(output[:,:1])
-        ax.plot(range(len(obs)), obs, color='tab:blue')
-        ax.plot(range(len(obs)), estim_mu, color='k')
-        ax.fill_between(range(len(obs)), estim_mu-estim_sigma, estim_mu+estim_sigma, color='k', alpha=0.2)
-        plt.savefig(save_path)
+            
+    # for some 8 samples out of the whole batch of length obs.size(0)
+    for i in range(min(8, obs.size(0))):
+        # Plot observation
+        plt.plot(range(len(obs[i])), obs[i], color='tab:blue', label='y_true')
+
+        # Plot estimation
+        if output.size(2) == 2:
+            # As a distribution (with uncertainty)
+            estim_mu, estim_sigma = output[i,:,0], torch.sqrt(output[i,:,1])
+            plt.plot(range(len(obs[i])), estim_mu, color='k', label='y_hat')
+            plt.fill_between(range(len(obs[i])), estim_mu-estim_sigma, estim_mu+estim_sigma, color='k', alpha=0.2)
+        
+        elif output.size(2) == 1:
+            # As a point estimation
+            plt.plot(range(len(obs[i])), output[i].squeeze(), color='k', label='y_hat')
+
+        plt.legend()
+        plt.savefig(f'{save_path}_s{i}.png')
         plt.close()
-    elif output.size(2) == 1:
-        ax.plot(range(len(obs)), obs, color='tab:blue')
-        ax.plot(range(len(obs)), output.squeeze(), color='k')
-        plt.savefig(save_path)
-        plt.close()
+
+
+def save_weights(self, modelpath, epoch=None):
+    """ Saves the current statedict of the nn.Module to modelpath. Warning: initial states ar
+        not saved in the current version (to do).
+
+    Parameters
+    ----------
+    modelpath : str
+        Path where to save the statedict. If not a full path, it assumes the file is in
+        ./models.
+    epoch     : int (optional)
+        If provided, it appends 'e{epoch}' to modelpath; useful to save the training progress.
+    """
+
+    if not os.path.exists('./models'):
+        os.mkdir('./models')
+
+    if epoch is not None:
+        modelpath = f'{modelpath.split(".")[0]}_e{epoch:02d}'
+    if '.pt' not in modelpath:
+        modelpath += '.pt'
+    if modelpath[0] != '/':
+        if './models' not in modelpath:
+            modelpath = './models/' + modelpath
+
+    torch.save(self.model.state_dict(), modelpath)
+
 
 
 
@@ -202,10 +271,10 @@ if __name__=='__main__':
 
     
     # Define training parameters
-    num_epochs      = 2 # 150
+    num_epochs      = 10 # 150
     batch_res       = 10    # Store and report loss every batch_res batches
-    batch_size      = 2 # 128   # batch_size = N_batch too # TODO: check this
-    n_batches       = 2 # 20
+    batch_size      = 30 # 128   # batch_size = N_batch too # TODO: check this
+    n_batches       = 20 # 20
     learning_rate   = 5e-4
     weight_decay    = 1e-5 
     loss_function   = torch.nn.GaussianNLLLoss() # GaussianNLL

paradigm/audit_gm.py
@@ -53,6 +53,8 @@ class AuditGenerativeModel:
         self.si_q = params["si_q"]
         self.si_r = params["si_r"]
 
+        if "N_ctx" not in params.keys(): self.N_ctx = 2 # context refers to being a std / dvt
+
     # Auxiliary samplers from goin.coin.GenerativeModel
 
     def _sample_N_(self, mu, si, size=1):
@@ -209,28 +211,23 @@ class AuditGenerativeModel:
         # d = self._sample_N_(0, self.si_d, (np.max(contexts)+1, contexts.shape[0]))
 
         # Sample params for each block
-        tau, lim = np.zeros((2, self.N_blocks)), np.zeros(
-            (2, self.N_blocks)
-        )  # 2 as for len({std, dvt})
+        tau, lim = np.zeros((2, self.N_blocks)), np.zeros((2, self.N_blocks))  # 2 as for len({std, dvt})
+
         for b in range(self.N_blocks):
             # Sample one pair of std/dvt lim values for each block
             mu_lim = self.sample_uniform_pair(self.tones_values)
 
-            for c in np.unique(contexts):  # np.unique(contexts)=range(2)
+            for c in range(self.N_ctx):  # 2 contexts: std or dvt
                 # Sample dynamics params for each context (std and dvt)
-                tau[c, b] = self._sample_TN_(
-                    1, 50, self.mu_tau, self.si_tau
-                ).item()  # A high boundary
-                lim[c, b] = self._sample_N_(
-                    mu_lim[c], self.si_lim
-                ).item()  # TODO: check values
+                tau[c, b] = self._sample_TN_(1, 50, self.mu_tau, self.si_tau).item()  # A high boundary
+                lim[c, b] = self._sample_N_(mu_lim[c], self.si_lim).item()  # TODO: check values
 
         # tau = self._sample_TN_(0, 1, self.mu_tau, self.si_tau, (np.max(contexts)+1, contexts.shape[0])) # TODO: check if okay to replace with len(np.unique(contexts))
         # lim = self._sample_N_(self.mu_lim, self.si_lim, (np.max(contexts)+1, contexts.shape[0]))
 
-        states = dict([(int(c), np.zeros(contexts.shape)) for c in np.unique(contexts)])
+        states = dict([(int(c), np.zeros(contexts.shape)) for c in range(self.N_ctx)])
 
-        for c in np.unique(contexts):  # np.unique(contexts)=range(2)
+        for c in range(self.N_ctx):  # self.N_ctx == 2
 
             # Initialize with a sample from distribution of mean and std the LGD stationary values
             # states[c][:,0] = self._sample_N_(d[c]/(1-a[c]), self.si_q/((1-a[c]**2)**.5), (contexts.shape[0], 1))
@@ -319,7 +316,7 @@ class AuditGenerativeModel:
         fig.tight_layout()
         plt.show()
 
-    def generate_batch(self, N_batch=None):
+    def generate_batch(self, N_batch=None, save=False):
         # Store latent rules and timbres, states and observations from N_batch batches
         # TODO: find a better way to store batches
 
@@ -372,18 +369,14 @@ class NonHierachicalAuditGM(AuditGenerativeModel):
         self, N_blocks=None, N_tones=None, mu_rho_ctx=None, si_rho_ctx=None
     ):
 
-        if N_blocks is None:
-            N_blocks = self.N_blocks
-        if N_tones is None:
-            N_tones = self.N_tones
-        if mu_rho_ctx is None:
-            mu_rho_ctx = self.mu_rho_ctx
-        if si_rho_ctx is None:
-            si_rho_ctx = self.si_rho_ctx
+        if N_blocks is None:    N_blocks    = self.N_blocks
+        if N_tones is None:     N_tones     = self.N_tones
+        if mu_rho_ctx is None:  mu_rho_ctx  = self.mu_rho_ctx
+        if si_rho_ctx is None:  si_rho_ctx  = self.si_rho_ctx
 
         # Get std/dvt contexts
         contexts = self.sample_contexts(
-            N=N_tones, N_ctx=2, mu_rho_ctx=mu_rho_ctx, si_rho_ctx=si_rho_ctx
+            N=N_tones, N_ctx=self.N_ctx, mu_rho_ctx=mu_rho_ctx, si_rho_ctx=si_rho_ctx
         )
         contexts = contexts.reshape((N_blocks, N_tones))
 
@@ -393,6 +386,7 @@ class NonHierachicalAuditGM(AuditGenerativeModel):
 
         # Flatten rules_long, contexts, (states, ) timbres and obs
         contexts = contexts.flatten()
+        pass
         states = dict([(key, states[key].flatten()) for key in states.keys()])
         obs = obs.flatten()
 
@@ -436,9 +430,7 @@ class HierarchicalAuditGM(AuditGenerativeModel):
             Sequence of rules associated with blocks for each block of N_blocks blocks
         """
 
-        return self.sample_contexts(
-            N_blocks, N_rules, mu_rho_rules, si_rho_rules, return_pi
-        )
+        return self.sample_contexts(N=N_blocks, N_ctx=N_rules, mu_rho_ctx=mu_rho_rules, si_rho_ctx=si_rho_rules, return_pi=return_pi)
 
     def sample_timbres(self, rules_seq, N_timbres, mu_rho_timbres, si_rho_timbres):
         """Sample timbres, mediated by a Markov chain transition process too
@@ -713,16 +705,10 @@ if __name__ == "__main__":
 
     gm = HierarchicalAuditGM(config_H)
 
-    rules, rules_long, dpos, timbres, timbres_long, contexts, states, obs = (
-        gm.generate_run()
-    )
-    rules_, rules_long_, dpos_, timbres_, timbres_long_, contexts_, states_, obs_ = (
-        gm.generate_batch(N_batch=2)
-    )
+    rules, rules_long, dpos, timbres, timbres_long, contexts, states, obs = gm.generate_run()
+    rules_, rules_long_, dpos_, timbres_, timbres_long_, contexts_, states_, obs_ = gm.generate_batch(N_batch=2)
 
-    gm.plot_contexts_rules_states_obs(
-        states[0][0 : gm.N_tones], states[1], obs, contexts, rules, dpos
-    )
+    gm.plot_contexts_rules_states_obs(states[0][0 : gm.N_tones], states[1], obs, contexts, rules, dpos)
     gm.plot_rules_dpos(rules, dpos)
     gm.plot_contexts_states_obs(
         contexts[0 : gm.N_tones],
@@ -749,9 +735,7 @@ if __name__ == "__main__":
     gm_NH = NonHierachicalAuditGM(config_NH)
 
     contexts_NH, states_NH, obs_NH = gm_NH.generate_run()
-    gm_NH.plot_contexts_states_obs(
-        contexts_NH, obs_NH, states_NH[0], states_NH[1], gm_NH.N_tones, figsize=(20, 6)
-    )
+    gm_NH.plot_contexts_states_obs(contexts_NH, obs_NH, states_NH[0], states_NH[1], gm_NH.N_tones, figsize=(20, 6))
 
     contexts_NH, states_NH, obs_NH = gm_NH.generate_batch(N_batch=2)
 