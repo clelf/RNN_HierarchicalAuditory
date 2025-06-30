sigmoid transform output mean; corrected VAE/VRNN KL part of loss (neg sign missing); recon loss to sum instead of mean to match KL loss; loss function defined at proper range location (was propagating across different trainings)

RNN/model.py
@@ -37,15 +37,15 @@ class SimpleRNN(nn.Module):
 
         output_last, _ = self.rnn(x, hx)  # output_seq is the sequence of final hidden states after the last layer / h_last is the final hidden state of each layer
         output_seq = self.output_layer(output_last)
+
+
         return output_seq # has last dimension = output_dim
     
-    def loss(self, target, x_output, loss_func, **kwargs): # torch.nn.GaussianNLLLoss()
-        out_estim_mu    = x_output[..., [0]]
+    def loss(self, target, x_output, loss_func): # torch.nn.GaussianNLLLoss()
+        out_estim_mu    = F.sigmoid(x_output[..., [0]])
         out_estim_var   = F.softplus(x_output[..., [1]]) + 1e-6 # Ensure the variance is positive
         
-        # In case more arguements are passed, depending on the loss_func considered
-        if kwargs: return loss_func(out_estim_mu, target, out_estim_var, **kwargs)
-        else: return loss_func(out_estim_mu, target, out_estim_var)
+        return loss_func(out_estim_mu, target, out_estim_var)
 
 
 class VAE(nn.Module):
@@ -112,35 +112,34 @@ class VAE(nn.Module):
         z = self.encoder(x, self.x_dim, self.hidden_units_dim)
 
         # Reparametrize the latent variable
-        latent_mu = self.fc_mu_latent(z)
-        latent_logvar = self.fc_logvar_latent(z)
-        z = self.reparameterize(latent_mu, latent_logvar)
+        mu_latent = self.fc_mu_latent(z)
+        logvar_latent = self.fc_logvar_latent(z)
+        z = self.reparameterize(mu_latent, logvar_latent)
 
         # Decode (reconstruct / generate)
         x_ = self.decoder(z, self.hidden_units_dim, self.x_dim)
 
-        return x_, latent_mu, latent_logvar
+
+        return x_, mu_latent, logvar_latent
 
 
-    def loss(self, forward_output, loss_func, **kwargs):
-        x_target, x_output, latent_mu, latent_logvar = forward_output
-        return self.vae_loss_function(self, x_target, x_output, latent_mu, latent_logvar, loss_func=loss_func, **kwargs)
+    def loss(self, forward_output, loss_func):
+        x_target, x_output, mu_latent, logvar_latent = forward_output
+        return self.vae_loss_function(self, x_target, x_output, mu_latent, logvar_latent, loss_func=loss_func)
 
 
-    def vae_loss_function(self, x_target, x_output, latent_mu, latent_logvar, loss_func, **kwargs):
+    def vae_loss_function(self, x_target, x_output, mu_latent, logvar_latent, loss_func):
         # (from pytorch's VAE implementation: https://github.com/pytorch/examples/blob/main/vae/main.py#L85)
         #Reconstruction + KL divergence losses summed over all elements and batch
-        out_estim_mu    = x_output[:, :, [0]]
+        out_estim_mu    = F.sigmoid(x_output[:, :, [0]])
         out_estim_var   = F.softplus(x_output[:, :, [1]]) + 1e-6 # Ensure the variance is stricly positive
-        if kwargs: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var, **kwargs)
-        else: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var)
-
+        recon_loss = loss_func(out_estim_mu, x_target, out_estim_var)
         
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
         # https://arxiv.org/abs/1312.6114
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        kl_loss = -0.5 * torch.sum(1 + latent_logvar - latent_mu.pow(2) - latent_logvar.exp())
+        kl_loss = -0.5 * torch.sum(1 + logvar_latent - mu_latent.pow(2) - logvar_latent.exp())
 
         return recon_loss + kl_loss
     
@@ -159,10 +158,11 @@ class VRNN(VAE): # Or rather, inherit from VAE
         self.phi_z_dim = phi_z_dim # h_dim
         self.phi_prior_dim = phi_prior_dim # h_dim
 
+        # NOTE: in VRNN paper all phi functions have 4 layers combined with ReLU
+
         # Encoder and decoder
         self.encoder = self.build_encoder(self.phi_x_dim + self.rnn_hidden_states_dim, self.latent_dim) 
         self.decoder = self.build_decoder(self.phi_z_dim + self.rnn_hidden_states_dim, self.output_dim) # shared with standard RNN (S.4 Models)
-
 
         # Feature extractors 
         self.phi_x = self.build_feature_extractor(self.x_dim, self.phi_x_dim)  # shared with standard RNN (S.4 Models)
@@ -193,8 +193,8 @@ class VRNN(VAE): # Or rather, inherit from VAE
 
         h_prev  = torch.zeros(self.rnn_n_layers, batch_size, self.rnn_hidden_states_dim, device=self.device)
         outputs = []
-        latent_mus = []
-        latent_logvars = []
+        mus_latent = []
+        logvars_latent = []
         prior_mus = []
         prior_logvars = []
 
@@ -205,11 +205,11 @@ class VRNN(VAE): # Or rather, inherit from VAE
             z_t = self.encoder(torch.cat([self.phi_x(x_t), h_prev[-1]], dim=-1)) # Eq. 9
 
             # Reparametrize the latent variable
-            latent_mu = self.fc_mu_latent(z_t)
-            latent_logvar = self.fc_logvar_latent(z_t)
-            latent_mus.append(latent_mu)
-            latent_logvars.append(latent_logvar)
-            z_t = self.reparameterize(latent_mu, latent_logvar)
+            mu_latent = self.fc_mu_latent(z_t)
+            logvar_latent = self.fc_logvar_latent(z_t)
+            mus_latent.append(mu_latent)
+            logvars_latent.append(logvar_latent)
+            z_t = self.reparameterize(mu_latent, logvar_latent)
 
             # Prior
             z_prior = self.phi_prior(h_prev[-1]) # Eq. 5
@@ -233,27 +233,25 @@ class VRNN(VAE): # Or rather, inherit from VAE
 
         # Stack across time
         outputs = torch.stack(outputs, dim=1)             # [B, T, x_dim]
-        latent_mus = torch.stack(latent_mus, dim=1)               # [B, T, latent_dim]
-        latent_logvars = torch.stack(latent_logvars, dim=1)       # [B, T, latent_dim]
+
+        mus_latent = torch.stack(mus_latent, dim=1)               # [B, T, latent_dim]
+        logvars_latent = torch.stack(logvars_latent, dim=1)       # [B, T, latent_dim]
         prior_mus = torch.stack(prior_mus, dim=1)         # [B, T, latent_dim]
-        prior_logvars = torch.stack(prior_logvars, dim=1) # [B, T, latent_dim]
-                
+        prior_logvars = torch.stack(prior_logvars, dim=1) # [B, T, latent_dim]      
 
-        return outputs, latent_mus, latent_logvars, prior_mus, prior_logvars
+        return outputs, mus_latent, logvars_latent, prior_mus, prior_logvars
     
     
-    def loss(self, x_target, forward_output, loss_func, **kwargs):
+    def loss(self, x_target, forward_output, loss_func):
         x_output, mu_latent, logvar_latent, mu_prior, logvar_prior = forward_output
-        return self.vrnn_loss(x_target, x_output, mu_latent, logvar_latent, mu_prior, logvar_prior, loss_func=loss_func, **kwargs)
+        return self.vrnn_loss(x_target, x_output, mu_latent, logvar_latent, mu_prior, logvar_prior, loss_func=loss_func)
     
 
-    def vrnn_loss(self, x_target, x_output, mu_latent, logvar_latent, mu_prior, logvar_prior, loss_func, **kwargs): # KL divergence
-        out_estim_mu = x_output[..., [0]]
-        out_estim_var = F.softplus(x_output[..., [1]]) + 1e-06
+    def vrnn_loss(self, x_target, x_output, mu_latent, logvar_latent, mu_prior, logvar_prior, loss_func): # KL divergence
+        out_estim_mu = F.sigmoid(x_output[:, :, [0]])
+        out_estim_var = F.softplus(x_output[:, :, [1]]) + 1e-06
 
-        # In case more arguements are passed, depending on the loss_func considered
-        if kwargs: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var, **kwargs)
-        else: recon_loss = loss_func(out_estim_mu, x_target, out_estim_var)
+        recon_loss = loss_func(out_estim_mu, x_target, out_estim_var)
         
         # KL divergence between two Gaussians per timestep & batch
         # KL(q||p) closed form between N(mu_latent, var_latent) and N(mu_prior, logvar_prior)
@@ -263,9 +261,10 @@ class VRNN(VAE): # Or rather, inherit from VAE
             - 1
         )
 
-        kl_loss = 0.5 * torch.sum(kl_element) # TODO: check if dim=1 is needed for torch.sum(..., dim=1)
+        # KL loss = - KL divergence
+        kl_loss =  - 0.5 * torch.sum(kl_element) 
 
-        return recon_loss + kl_loss
+        return recon_loss + kl_loss # Eq. 11 : - KL divergence + log posterior
 
 
 if __name__ == '__main__':
@@ -278,7 +277,6 @@ if __name__ == '__main__':
     # Input and output dimensions
     input_dim   = 1 # number of features in each observation: 1 --> need to write x = x.unsqueeze(-1)  at one point
     dim_out_obs = 2 # from goin: learn the sufficient statistics mu and var
-    dim_out_ctx = 3 # from goin. nb_max_ctx + 1 = 2+1
     # RNN bit
     output_dim      = input_dim
     rnn_hidden_dim  = 64 # from goin. could also take varying values like [2**n for n in range(1, 9)]
@@ -291,7 +289,7 @@ if __name__ == '__main__':
 
         
     # Define model
-    rnn     = SimpleRNN(x_dim=input_dim, hidden_dim=rnn_hidden_dim, n_layers=rnn_n_layers)
+    rnn     = SimpleRNN(x_dim=input_dim, output_dim=dim_out_obs, hidden_dim=rnn_hidden_dim, n_layers=rnn_n_layers)
     vrnn    = VRNN(x_dim=input_dim, latent_dim=latent_dim, phi_x_dim=phi_x_dim, phi_z_dim=phi_z_dim, phi_prior_dim=phi_prior_dim, rnn_hidden_states_dim=rnn_hidden_dim, rnn_n_layers=rnn_n_layers)
     
     y = [1., 2., 1., 2.]

RNN/pipeline.py
@@ -67,6 +67,10 @@ class MinMaxScaler():
 
 
 def train(model, n_batches, num_epochs, batch_res, epoch_res, optimizer, lr, lr_id, gm_name, data_config, kalman=False):
+    
+    # Define loss instance
+    loss_function   = torch.nn.GaussianNLLLoss(reduction='sum') # GaussianNLL
+
 
     # Define data generative model
     if gm_name == 'NonHierarchicalGM': gm = NonHierachicalAuditGM(data_config)
@@ -74,6 +78,7 @@ def train(model, n_batches, num_epochs, batch_res, epoch_res, optimizer, lr, lr_
     else: raise ValueError("Invalid GM name")
 
     # Prepare to save the results
+    lr_title = f"Learning rate: {lr:>6.0e}"
     save_path = Path(f'/home/clevyfidel/Documents/Workspace/RNN_paradigm/training_results/N_ctx_{data_config["N_ctx"]}/{model.name}/')
     if not os.path.exists(save_path):
         os.makedirs(save_path)
@@ -101,8 +106,8 @@ def train(model, n_batches, num_epochs, batch_res, epoch_res, optimizer, lr, lr_
             y = torch.tensor(y, dtype=torch.float, requires_grad=False).unsqueeze(2).to(DEVICE)
 
             # Transform data
-            minmax = MinMaxScaler()
-            x = minmax.fit_normalize(y, margin=data_config["si_r"]+data_config["si_q"])
+            minmax_train = MinMaxScaler()
+            x = minmax_train.fit_normalize(y, margin=data_config["si_r"]+data_config["si_q"])
 
             # Call model
             model_output = model(x) # can contain output(, mu_latent, logvar_latent(, mu_prior, logvar_prior))
@@ -159,7 +164,7 @@ def train(model, n_batches, num_epochs, batch_res, epoch_res, optimizer, lr, lr_
                 if model.name=='vrnn': model_output_dist = model_output[0]
                 else: model_output_dist = model_output
                 x_denorm    = minmax_valid.denormalize_mean(x).squeeze()
-                estim_mu    = minmax_valid.denormalize_mean(model_output_dist[...,0])
+                estim_mu    = minmax_valid.denormalize_mean(F.sigmoid(model_output_dist[...,0]))
                 estim_var   = minmax_valid.denormalize_var(F.softplus(model_output_dist[..., 1]) + 1e-6)
                 estim_sigma = torch.sqrt(estim_var)
                 valid_sigma.append(estim_sigma)
@@ -176,7 +181,7 @@ def train(model, n_batches, num_epochs, batch_res, epoch_res, optimizer, lr, lr_
 
             # Save valid samples
             if epoch % epoch_res == epoch_res-1:                         
-                plot_samples(x_denorm, estim_mu, estim_sigma, save_path=f'{save_path}/samples/lr{lr_id}-epoch-{epoch:0>3}_samples', kalman_mu=kalman_mu, kalman_sigma=kalman_sigma)
+                plot_samples(x_denorm, estim_mu, estim_sigma, save_path=f'{save_path}/samples/lr{lr_id}-epoch-{epoch:0>3}_samples', title=lr_title, kalman_mu=kalman_mu, kalman_sigma=kalman_sigma)
 
             avg_valid_loss = np.mean(valid_losses)
             avg_valid_mse = np.mean(valid_mse)
@@ -198,14 +203,13 @@ def train(model, n_batches, num_epochs, batch_res, epoch_res, optimizer, lr, lr_
             f.write(f'{sprint}\n')
 
     lossplotfile = save_path/f'loss_trainvalid_lr{lr_id}.png'
-    plot_losses(train_steps, valid_steps, train_losses_report, valid_losses_report, x_label='Training steps', y_label='Loss', save_path=lossplotfile)
-
-    plot_var(valid_steps, valid_sigma_report, save_path/f'variance_valid_lr{lr_id}.png')
+    plot_losses(train_steps, valid_steps, train_losses_report, valid_losses_report, x_label='Training steps', y_label='Loss', title=lr_title, save_path=lossplotfile)
+    plot_var(valid_steps, valid_sigma_report, title=lr_title, save_path=save_path/f'variance_valid_lr{lr_id}.png')
 
     # Save model's weights
     torch.save(model.state_dict(), f'{save_path}/lr{lr_id}_weights.pth')
 
-    return minmax
+    return minmax_train
  
 
 
@@ -213,12 +217,13 @@ def test():
     return NotImplementedError
 
 
-def plot_var(valid_steps, valid_sigma, save_path):
+def plot_var(valid_steps, valid_sigma, title, save_path):
     plt.figure(figsize=(10, 5))
     plt.plot(valid_steps, valid_sigma, label="valid std")
     plt.xlabel("Training steps")
     plt.ylabel("Valid variance (std)")
     plt.legend()
+    plt.title(title)
     plt.savefig(save_path)
     plt.close()
 
@@ -233,7 +238,7 @@ def plot_losses(train_steps, valid_steps, train_losses_report, valid_losses_repo
     plt.close()
 
 
-def plot_samples(obs, estim_mu, estim_sigma, save_path, kalman_mu=None, kalman_sigma=None):
+def plot_samples(obs, estim_mu, estim_sigma, save_path, title=None, kalman_mu=None, kalman_sigma=None):
     obs = obs.squeeze() # for safety
             
     # for some 8 samples out of the whole batch of length obs.size(0)
@@ -255,6 +260,7 @@ def plot_samples(obs, estim_mu, estim_sigma, save_path, kalman_mu=None, kalman_s
             plt.fill_between(range(len(kalman_mu[i])), kalman_mu[i]-kalman_sigma[i], kalman_mu[i]+kalman_sigma[i], color='green', alpha=0.2)
 
         plt.legend()
+        plt.title(title)
         plt.savefig(f'{save_path}_s{id}.png')
         plt.close()
 
@@ -269,7 +275,7 @@ if __name__=='__main__':
     dim_out_obs = 2 # learn the sufficient statistics mu and var
     # RNN bit
     output_dim      = input_dim
-    rnn_hidden_dim  = 8 # 64 # from goin. could also take varying values like [2**n for n in range(1, 9)]
+    rnn_hidden_dim  = 8 # 64 # from goin. could also take varying values like [2**n for n in range(1, 9)] # In VRNN paper: 2000
     rnn_n_layers    = 1 # from goin
     # VRNN specific
     latent_dim      = 4 # 16 # needs to be < input_dim :thinks: --> in a VAE, yes, but in a VRNN, needs to be <input_dim + rnn_hidden_dim
@@ -286,7 +292,6 @@ if __name__=='__main__':
     n_batches       = 20 # 20
     learning_rate   = 5e-4
     weight_decay    = 1e-5 
-    loss_function   = torch.nn.GaussianNLLLoss() # GaussianNLL
 
 
     # Define models
@@ -313,13 +318,12 @@ if __name__=='__main__':
         "si_r": 2,  # measurement noise
     }
 
-    for lr_id, learning_rate in enumerate([0.001, 0.00001]):
-    # for lr_id, learning_rate in enumerate([0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]):
-        # for model in [vrnn, rnn]:
-        for model in [vrnn]:
-            optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-            train(model, n_batches=n_batches, num_epochs=num_epochs, batch_res=batch_res, epoch_res=epoch_res, optimizer=optimizer, 
-                  lr=learning_rate, lr_id=lr_id, gm_name=gm_name, data_config=config_NH, kalman=True)
+    for lr_id, learning_rate in enumerate([0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001]): # TODO: delete 0.1, 0.5
+        if lr_id in [7, 8]:
+            for model in [rnn, vrnn]:
+                optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+                train(model, n_batches=n_batches, num_epochs=num_epochs, batch_res=batch_res, epoch_res=epoch_res, optimizer=optimizer, 
+                    lr=learning_rate, lr_id=lr_id, gm_name=gm_name, data_config=config_NH, kalman=True)
 
 
             # test(...)