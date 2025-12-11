cf5d6b38f8f78468b3655c52c4537db9119548f3
clelf <clementinelvfd@gmail.com>
2025-12-11 18:16:44 +0100

    just one process (N_ctx=1)

--- RNN/pipeline_multi.py
+++ RNN/pipeline_multi.py
@@ -10,7 +10,7 @@ FREQ_MAX = 1650
 
 if __name__=='__main__':
 
-    unit_test = True
+    unit_test = False
 
     # DEFINE MODEL AND TRAINING PARAMETERS
     model_config = {
@@ -20,21 +20,21 @@ if __name__=='__main__':
         "model": ['rnn', 'vrnn'],  #: 'rnn', 'vrnn'
 
         # Learning rates to test
-        "learning_rates": [0.05, 0.01, 0.005, 0.001, 0.0005] if not unit_test else [0.01, 0.005],  # list of learning rates to try
+        "learning_rates":  [0.05, 0.01, 0.005, 0.001, 0.0005] if not unit_test else [0.01, 0.005],  # list of learning rates to try # [0.01, 0.005], #
 
         # Input and output dimensions
         "input_dim": 1,  # number of features in each observation: 1 --> need to write y_norm = y_norm.unsqueeze(-1) at one point
         "output_dim": 2,  # learn the sufficient statistics mu and var
 
         # RNN configuration
-        "rnn_hidden_dim": [8, 16, 32, 64] if not unit_test else [8, 16],  # prev: 8
+        "rnn_hidden_dim": [8, 16, 32, 64] if not unit_test else [8, 16],  # prev: 8  # [16], #
         "rnn_n_layers": 1,  # number of RNN layers
 
         # Training parameters
         "num_epochs": 100 if not unit_test else 10, # TODO: 250 / 100,  # number of epochs (TEST: 2)
         "epoch_res": 20 if not unit_test else 10,  # report results every epoch_res epochs
         "batch_res": 16 if not unit_test else 2,  # store and report loss every batch_res batches
-        "batch_size": 1000 if not unit_test else 5, # 5, # TODO: 1000,  # batch size (TEST: 5)
+        "batch_size": 1000 if not unit_test else 64, # 5, # TODO: 1000,  # batch size (TEST: 5)
         "n_batches": 32 if not unit_test else 2,  # number of batches # TODO: 32
         "weight_decay": 1e-5,  # weight decay for optimizer
 
@@ -42,7 +42,7 @@ if __name__=='__main__':
         "n_trials": 1000,  # single tones
 
         # Testing parameters
-        "batch_size_test": 1000 if not unit_test else 5, # 5 # batch size during testing # TODO: 1000 (TEST: 10)
+        "batch_size_test": 1000 if not unit_test else 64, # 5 # batch size during testing # TODO: 1000 (TEST: 10)
 
         # Visualization parameters
         "seq_len_viz": 125,  # or None
@@ -62,7 +62,7 @@ if __name__=='__main__':
         max_cores = max(1, cpu_count() // 2) if not unit_test else 1 # Use half of local CPUs
     
     data_config = {
-        "gm_name": "HierarchicalGM",
+        "gm_name": "NonHierarchicalGM",
         "N_ctx": 1,
         # "N_batch": n_batches,
         "N_samples": model_config['batch_size'],
@@ -100,7 +100,6 @@ if __name__=='__main__':
         "si_rho_rules": 0.05,
     }
     
-
     
     # PARAMETERS TESTING
     add_data_params_baseline = {
@@ -112,26 +111,31 @@ if __name__=='__main__':
     if data_config["params_testing"]:
         data_config.update(add_data_params_baseline)
 
+    
+    # TRAINING MODEL WITH NON HIERARCHICAL GM AND SINGLE CONTEXT
+    print("Running N_ctx = 1")
+    pipeline_model(model_config, data_config)
 
-    #### RUNNING DIFFERENT GM
 
-    for N_ctx in [1, 2]:
-        print("Running N_ctx =", N_ctx)
-        data_config["N_ctx"] = N_ctx  # Update N_ctx in data_config
-        if data_config["N_ctx"] > 1:
-            data_config.update(add_multi_context_params)
+    # #### RUNNING DIFFERENT GMS (OBSOLETE)
 
-        for gm_name in ['NonHierarchicalGM', 'HierarchicalGM']:
-            if gm_name == 'HierarchicalGM' and N_ctx == 1:
-                continue
+    # for N_ctx in [1, 2]:
+    #     print("Running N_ctx =", N_ctx)
+    #     data_config["N_ctx"] = N_ctx  # Update N_ctx in data_config
+    #     if data_config["N_ctx"] > 1:
+    #         data_config.update(add_multi_context_params)
+
+    #     for gm_name in ['NonHierarchicalGM', 'HierarchicalGM']:
+    #         if gm_name == 'HierarchicalGM' and N_ctx == 1:
+    #             continue
             
-            print("Running GM = ", gm_name)
-            data_config['gm_name'] = gm_name
+    #         print("Running GM = ", gm_name)
+    #         data_config['gm_name'] = gm_name
             
-            if data_config["gm_name"] == "HierarchicalGM":
-                data_config.update(add_hierarchical_params)
+    #         if data_config["gm_name"] == "HierarchicalGM":
+    #             data_config.update(add_hierarchical_params)
             
-            pipeline_model(model_config, data_config)
+    #         pipeline_model(model_config, data_config)
 
 
 
